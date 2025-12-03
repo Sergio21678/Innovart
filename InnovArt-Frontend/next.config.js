b799818a73d47310dd12d9d83559c39868b45f0c
Sergio21678 <sergio.vilca@tecsup.edu.pe>
2025-12-03 13:09:54 -0500

    configurar export

--- InnovArt-Frontend/next.config.js
+++ InnovArt-Frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   env: {
     // Permite sobreescribir en Render/Vercel; fallback local para desarrollo
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
